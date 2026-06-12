0c3ffdaf42d243b5e6a889c83fb284311b2eb01b
CarbonAxis Exchange <info@carbonaxisexchange.com>
2026-06-12 12:00:25 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-
+import nodemailer from "nodemailer";
 dotenv.config();
 
 const app = express();
