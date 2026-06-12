6dca5b702b72b4e186e786f9f0a009daafeb4fcb
CarbonAxis Exchange <info@carbonaxisexchange.com>
2026-06-12 21:05:13 +0500

    Update server.js

--- server.js
+++ server.js
@@ -311,3 +311,8 @@ app.post("/api/broker-inquiry", async (req, res) => {
     });
   }
 });
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
