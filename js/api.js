a7b2767345d76d5e7ca8fb06522913ee32e5f642
Muhammad Zain <zaid.hamid11995@gmail.com>
2026-06-30 15:52:29 +0500

    Add registration page

---
+++ js/api.js
@@ -0,0 +1,9 @@
+const API = {
+  BASE: "https://carbonaxis-exchange.onrender.com/api",
+
+  login: "/login",
+  register: "/register",
+  profile: "/profile",
+  projects: "/projects",
+  watchlist: "/watchlist"
+};
