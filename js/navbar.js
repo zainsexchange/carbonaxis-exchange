8aebe02606227acc8e89295ffa1d5d00544c9746
Muhammad Zain <zaid.hamid11995@gmail.com>
2026-07-04 11:55:21 +0500

    Fix navbar user name and early access links

--- js/navbar.js
+++ js/navbar.js
@@ -28,6 +28,25 @@ document.addEventListener("DOMContentLoaded", () => {
       notificationMenu.classList.toggle("active");
     });
   }
+  const user = JSON.parse(localStorage.getItem("user"));
+
+if (user && user.name) {
+  const avatar = document.querySelector(".profile-avatar");
+  const accountText = document.querySelector(".profile-btn span:nth-child(2)");
+
+  if (avatar) {
+    avatar.innerText = user.name
+      .split(" ")
+      .map(n => n[0])
+      .join("")
+      .substring(0, 2)
+      .toUpperCase();
+  }
+
+  if (accountText) {
+    accountText.innerText = user.name;
+  }
+}
 
   document.addEventListener("click", () => {
     if (profileMenu) {
